22d167412c46610f968e1de88858423a8217a6ab
it-ddt <45958272+it-ddt@users.noreply.github.com>
2023-10-21 13:21:05 +0300

    lesson 2

--- views.py
+++ views.py
@@ -1,15 +1,26 @@
-from django.shortcuts import render
-from django.http import HttpResponse
+from django.shortcuts import render, redirect
+from django.http import HttpResponse, HttpResponseNotAllowed
 from datetime import datetime
+
+users = ['Вася Питонов', 'Петя Гадюкин']
 
 
 def index(request):
-    return render(request, 'app/index.html')
-
-
-def greet(request):
-    user_name = request.GET['user_name']
     context = {
-        'user_name': user_name
+        'users': users
     }
-    return render(request, 'app/greet.html', context)
+    return render(request, 'app/index.html', context)
+
+
+def add(request):
+    if request.method == 'GET':
+        return render(request, 'app/add.html')
+    elif request.method == 'POST':
+        user = request.POST.get('user_name', False)
+        users.append(user)
+        return redirect('index')
+    else:
+        return HttpResponseNotAllowed(
+            ['POST', 'GET'],
+            content='Ошибка Этот метод не разрешен!'
+        )
